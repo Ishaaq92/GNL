4ba6886615229b665ad036969a0eddb92fc6c3ef
Ishaaq Ahmed <a.ishaaq@icloud.com>
2024-12-16 12:16:43 +0000

    There's a chance it doesn't work because I made some changes in trying to make the gnl 25 lines and under.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ishaaq <ishaaq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 11:43:18 by ishaaq            #+#    #+#             */
-/*   Updated: 2024/12/16 11:13:30 by ishaaq           ###   ########.fr       */
+/*   Updated: 2024/12/16 12:15:34 by ishaaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,19 @@ char	*extend(char *str, char *buffer, ssize_t num_read)
 	free(str);
 	return (new_str);
 }
-
+char	*cleanup(char *text, char *line)
+{
+	if (text[0] != '\0')
+	{
+		line = ft_substr(text, 0, countsubstr(text, 0, '\0'));
+		free(text);
+		text = NULL;
+		return line;
+	}
+	free(text);
+	text = NULL;
+	return NULL;  
+}
 char	*get_next_line(int fd)
 {
 	ssize_t		num_read;
@@ -77,20 +89,24 @@ char	*get_next_line(int fd)
 			return (NULL);
 		text[0] = '\0';
 	}
-	while ((newline_pos = ft_strchri(text, '\n', 0)) == -1)
+	newline_pos = ft_strchri(text, '\n', 0);
+	while (newline_pos == -1)
 	{
 		num_read = read(fd, buffer, BUFFER_SIZE);
 		if (num_read <= 0)
 			break;
 		buffer[num_read] = '\0';
 		text = extend(text, buffer, num_read);
+		newline_pos = ft_strchri(text, '\n', 0);
 	}
-	if ((newline_pos = ft_strchri(text, '\n', 0)) >= 0)
+	if (newline_pos  >= 0)
 	{
 		line = ft_substr(text, 0, countsubstr(text, 0, '\n') + 1);
 		text = ft_truncate(text, newline_pos + 1);
 		return (line);
 	}
+	// line = NULL;
+	// return (cleanup(text, line));
 	if (text[0] != '\0')
 	{
 		line = ft_substr(text, 0, countsubstr(text, 0, '\0'));
@@ -100,26 +116,37 @@ char	*get_next_line(int fd)
 	}
 	free(text);
 	text = NULL;
-	return NULL; // No more content to read
-	// while ((num_read = read(fd, buffer, BUFFER_SIZE)) > 0)
-	// {
-	// 	buffer[num_read] = '\0';
-	// 	text = extend(text, buffer, num_read);
-	// 	if (!text)
-	// 	{
-	// 		free(text);
-	// 		return (NULL);
-	// 	}
-	// 	if ((newline_pos = ft_strchri(text, '\n', 0)) >= 0)
-	// 	{
-	// 		line = ft_substr(text, 0, countsubstr(text, 0, '\n') + 1);
-	// 		text = ft_truncate(text, newline_pos + 1);
-	// 		return (line);
-	// 	}
-	// }
-	// if (text[0] != '\0') // Remaining content after EOF
-	// {
-	// 	line = ft_substr(text, 0, countsubstr(text, 0, '\0'));
-	// 	free(text);
-	// 	text = NULL;
-	// 	retur
+	return NULL; 
+}
+
+// int main(int ac, char *av[])
+// {
+// 	if (ac < 2)
+// 	{
+// 		printf("Usage: %s <filename>\n", av[0]);
+// 		return (1);
+// 	}
+
+// 	int fd = open(av[1], O_RDONLY);
+// 	if (fd < 0)
+// 	{
+// 		perror("Error opening file");
+// 		return (1);
+// 	}
+
+// 	char *line;
+// 	while ((line = get_next_line(fd)) != NULL)
+// 	{
+// 		printf("%s", line); // Print the line (newlines are preserved)
+// 		free(line);         // Free each line after use
+// 	}
+// 	// printf("%s", get_next_line(fd));
+	
+// 	// line = malloc(sizeof(char)* 12);
+// 	// line = "hello world";
+// 	// line[11] = '\0';
+// 	// printf("%s\n", ft_truncate(line, 1));
+	
+// 	close(fd);
+// 	return (0);
+// }
